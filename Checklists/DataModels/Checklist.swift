dafa6dc4db96a03c9ec34b35ab41a2a1cd19e128
Neatwhiskey <neatwhiskey24@gmail.com>
2022-10-24 00:57:12 +0100

    g

--- Checklists/DataModels/Checklist.swift
+++ Checklists/DataModels/Checklist.swift
@@ -11,8 +11,20 @@ class Checklist: NSObject, Codable{
   var name = ""
   var items = [ChecklistItem]()
   
+  func countUncheckedItems() -> Int{
+    return items.reduce(0){
+      count, item in count + (item.checked ? 0:1)
+    }
+//    var count = 0
+//    for item in items where !item.checked{
+//      count += 1
+//    }
+//    return count
+  }
+  
   init(name: String) {
     self.name = name
     super.init()
   }
+  
 }
